77e947f54f978e62e891f09eed5034fc3f2b600c
Lucas Nogueira <lucasnogueira@outlook.com.br>
2016-10-16 11:47:58 -0300

    refactory layout scroll

---
+++ script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  slidesPerView: 'auto',
+  paginationClickable: true,
+  spaceBetween: 30,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev'
+});
