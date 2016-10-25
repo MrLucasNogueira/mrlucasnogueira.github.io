35aefb1ec10a4b946580074c6320ab291dc0ea4d
Lucas Nogueira <lucasnogueira@outlook.com.br>
2016-10-25 17:11:20 -0300

    update section softwares

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ var swiper = new Swiper('.swiper-container', {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+function load(){
+  
+  var canvas = document.getElementById("canvas");
+  var classname = document.getElementsByClassName('videoCall');
+  
+  canvas.addEventListener("click", function(e){
+    e.target.style.zIndex = -10;
+    canvas.firstElementChild.src = '';
+  });
+
+  for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', function(){
+     
+        canvas.firstElementChild.src = this.getAttribute('src');
+      
+      canvas.style.zIndex = 3;
+    });
+  }
+
+}
+
+load();
